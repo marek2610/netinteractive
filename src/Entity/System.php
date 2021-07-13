4b13d8d9f7a203581835af887a8c6dc743f87d3b
Marek Wesołowski - HCI24.pl <wesolowski.marek@wp.pl>
2021-07-13 18:35:58 +0200

    Encje
    1. Programowanie (PHP, JS, inne)
    2. System (API, CLI, UI)

---
+++ src/Entity/System.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SystemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SystemRepository::class)
+ */
+class System
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nazwa;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNazwa(): ?string
+    {
+        return $this->nazwa;
+    }
+
+    public function setNazwa(string $nazwa): self
+    {
+        $this->nazwa = $nazwa;
+
+        return $this;
+    }
+}
