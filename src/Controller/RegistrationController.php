43e60bc7cef38a2d9caffbd293d5cf7b0f011dc7
Marek Wesołowski - HCI24.pl <wesolowski.marek@wp.pl>
2021-07-17 10:36:23 +0200

    Drobne poprawki

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -78,19 +78,6 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            # @ do osoby pełnoletniej z przywitaniem
-            // if(intval($interval->y) > 18){
-            //     // generate a signed url and email it to the user
-            //     $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-            //     (new TemplatedEmail())
-            //     ->from(new Address('no-reply@netinteractive.test', 'Test www.netinteractive.test'))
-            //     ->to($user->getEmail())
-            //     ->subject('Please Confirm your Email')
-            //     ->htmlTemplate('registration/confirmation_email.html.twig')
-            //     );
-            // }
-            // do anything else you need here, like send an email
-
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
                 $request,
